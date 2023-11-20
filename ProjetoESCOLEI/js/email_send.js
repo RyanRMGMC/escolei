c0c7a5a1c09ee07733338fd500b2f3e518381886
SWind2003 <85123800+RyanRMGMC@users.noreply.github.com>
2023-11-20 19:38:44 -0300

    Add files via upload

---
+++ ProjetoESCOLEI/js/email_send.js
@@ -0,0 +1,28 @@
+function enviarEmail() {
+  const emailInput = document.getElementById('emailInput');
+  const userEmail = emailInput.value;
+
+  // Faz uma solicitação ao servidor Node.js
+  fetch(`http://localhost:4000/enviar-email?destinatario=${userEmail}`)
+    .catch(response => {
+      console.log(response);
+      // Verifica o status da resposta
+      if (response.ok) {
+        // Resposta bem-sucedida
+        return response.text(); // ou response.json() se estiver retornando JSON
+      } else {
+        // Resposta com erro
+        console.error('Erro de rede ou servidor:', response.status, response.statusText);
+        throw new Error('Erro ao enviar e-mail');
+      }
+    })
+    .catch(data => {
+      // Exibe o alerta com base na resposta do servidor
+      console.log('Resposta do servidor:', data);
+      alert("Aguarde o recebimento."); // Altere conforme a estrutura da resposta do servidor
+    })
+    .catch(error => {
+      // Erro durante a chamada fetch (ignorando)
+      console.error('Erro durante o fetch:', error);
+    });
+}
