97f252bf1d7c6a7acc14efb4e73fe30a5ce84ece
SWind2003 <85123800+RyanRMGMC@users.noreply.github.com>
2023-11-20 19:50:24 -0300

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const nodemailer = require('nodemailer');
+const express = require('express');
+const email_send = express();
+const fs = require('fs');
+
+const port = 4000;
+
+// Configurações do transporte de e-mail usando SMTP
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'escoleiedu@gmail.com',
+    pass: 'ksaa cqtw schz nnhe', // Use a senha de aplicativo aqui
+  },
+});
+
+// Configurações do e-mail
+email_send.get('/enviar-email', (req, res) => {
+  const destinatario = req.query.destinatario;
+
+  const mailOptions = {
+    from: 'escoleiedu@gmail.com',
+    to: destinatario,
+    subject: 'Materiais de Estudo',
+    text: 'Olá! Segue arquivo com materiais de estudo do Portal ESCOLEI!',
+    attachments: [
+      {
+        filename: 'materiais_de_estudo.zip',
+        content: fs.createReadStream('./ProjetoESCOLEI/materiais_de_estudo/artigos_definidos_e_indefinidos/materiais_de_estudo.zip')
+      }
+    ]
+  };
+
+    // Envia o e-mail
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (!error) {
+        console.log('E-mail enviado:', info.response);
+        res.send('E-mail enviado com sucesso!');
+      } else {
+        console.error('Erro ao enviar e-mail:', error);
+        res.send('Erro ao enviar e-mail');
+      }
+  });
+});
+
+email_send.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
